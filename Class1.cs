889f8c53bf774f8b7d2ea54a9c8d301c4c3d63f9
Michael <mhouse@my.dom.edu>
2024-09-02 17:43:02 -0500

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstClass1;
+public class Patient
+{
+    // Properties
+    public string Name { get; set; }
+    public string Gender { get; set; }
+    public string BloodType { get; set; }
+    public int Age {get; set; }
+    public int MedicalRecordNumber { get; set; }
+
+
+
+    public Patient(string name,
+                   int age,
+                   string gender,
+                   string bloodtype);
+    }
+
+
+    
